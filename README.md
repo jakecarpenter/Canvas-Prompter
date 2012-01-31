8a5ccdac19429448493744facc8a496f2b48edb9
Jake Carpenter <jake@jakecarpenter.com>
2012-01-31 11:01:59 -0900

    basic functionality, animation coming soon.

---
+++ README.md
@@ -0,0 +1,6 @@
+Canvas Prompter is intended to be a simple teleprompter, it should work in the browser on mac, pc, and iOS/android
+
+Features I'd like to add:
+*Remote Control
+*Text from URL or file
+
--- canvasprompter.js
+++ canvasprompter.js
@@ -1,14 +1,41 @@
 var PROMPTER = function(options){
 	//if no options, great we'll do it all by ourself.
 	var options = options || {};
+		options.text = options.text || {};
+	
+	//first, what text are we showing the user?
+	var script = options.script || 
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed augue sem,"+
+	"porta id pharetra sit amet, dignissim egestas orci. Duis iaculis luctus"+
+	"vehicula. Fusce orci sapien, pharetra ac molestie eu, tincidunt sed ipsum. "+
+	"Integer sit amet dolor eu est viverra ultrices. Suspendisse potenti. "+
+	"Nullam semper neque quis odio laoreet consectetur egestas eros pretium. "+
+	"Praesent accumsan dictum pretium. Morbi tristique nulla sit amet nisl "+
+	"lacinia et cursus neque molestie. ";
 	
 	//orietation is for tablet devices.
 	var orientation = options.orientation || 90;
 	
 	//default width & height, get monkeyed with when orientation changes.
-	var width = options.width || 1024;
-	var height = options.height || 748;
-				
+	var width = options.width || 700;
+	var height = options.height || 450;
+	
+	//initial text settings, can be changed anytime.
+	var text = {};
+		text.x = 5;
+		text.height = 24;
+		text.font = options.text.font || "bold "+ text.height +"px sans-serif";
+		text.baseline = options.text.baseline || "top";
+		text.style = "#fff";
+
+	//caret/guidline setup.
+	var caret = {};
+		caret.color = "yellow";
+		caret.x = 10;
+		caret.y = 200;
+		caret.height = 40;
+		caret.width = 40;
+		
 	//if there was a container element specified, use it, otherwise make our own
 	var containerElement = options.container || function(){
 		var c = document.createElement('div');
@@ -16,6 +43,12 @@ var PROMPTER = function(options){
 		document.body.appendChild(c);
 		return c;
 	}();
+	
+	//motion stuff
+	var paused = false;
+	//we need to know if our overlay or script setup has changed and needs a redraw (ie, its stale)
+	var overlayStale = true;
+	var scriptStale = true;
 	
 	//we're going to use two canvases, one for the script text, and one for the overlay.		
 			
@@ -26,21 +59,19 @@ var PROMPTER = function(options){
 		c.setAttribute('class', 'scriptCan');
 		c.setAttribute('height', height);
 		c.setAttribute('width', width);
-		//c.setAttribute('z-index', 0);
 		containerElement.appendChild(c);
 		return c;
 	}();
 				
 	var scriptContext = scriptCanvas.getContext('2d');
 	
-	// script canvas element stuff.
+	// overlay canvas element stuff.
 	var overlayCanvas = options.overlayCanvas || function(){
 		var c = document.createElement('canvas');
 		c.setAttribute('id', 'overlayCanvas');
 		c.setAttribute('class', 'overlayCan');
 		c.setAttribute('height', height);
 		c.setAttribute('width', width);
-		//c.setAttribute('z-index', 0);
 		containerElement.appendChild(c);
 		return c;
 	}();
@@ -50,31 +81,149 @@ var PROMPTER = function(options){
 	//drawing/refreshing stuff here
 	//draw is where the magic happens.
 	var draw = function(){
+		if(paused){
+			return;
+		}
+		if(overlayStale == true){
+			overlayContext.clearRect(0,0,width, height);
+			drawCaret();
+			
+			//we've redrawn/refigured the overlay, so its no longer stale
+			overlayStale = false;
+		}
+		
+		if(scriptStale == true){
+			scriptText = textLines(script, width - caret.width);
+		}
+		
+		//clear the canvas
+		scriptContext.clearRect(0,0,width, height);
+		
+		//setup text: 
+		scriptContext.font = text.font;
+		scriptContext.fillStyle = text.style;
+		scriptContext.textBaseline = text.baseline;
+
+		var lineOffsetY = 0;
+		
+		for(var line in scriptText){
+			scriptContext.fillText(scriptText[line], text.x + caret.width, lineOffsetY);
+			lineOffsetY += text.height;	
+		}
+		//scriptContext.fillText(text, 10, 10);
 		
 	};
 	
 	var flipHorizontal = function(){
-		overlayCanvas.scale(-1,1);
-		overlayCanvas.translate(-overlayCanvas.width,0);
+		overlayContext.scale(-1,1);
+		overlayContext.translate(-overlayCanvas.width,0);
+		overlayStale = true;
 		
-		scriptCanvas.scale(-1,1);
-		scriptCanvas.translate(-scriptCanvas.width,0);
+		scriptContext.scale(-1,1);
+		scriptContext.translate(-scriptCanvas.width,0);
 	};
 	
 	var flipVertical = function(){
-		overlayCanvas.scale(1,-1);
-		overlayCanvas.translate(0,-overlayCanvas.height);
+		overlayContext.scale(1,-1);
+		overlayContext.translate(0,-overlayCanvas.height);
+		overlayStale = true;
 		
-		scriptCanvas.scale(1,-1);
-	   	scriptCanvas.translate(0,-scriptCanvas.height);
+		scriptContext.scale(1,-1);
+	   	scriptContext.translate(0,-scriptCanvas.height);
+
+	};
+	
+	/*
+	 * basically a pause. resumes at original speed.
+	 */
+	var stopStart = function(){
+		
 	};
 	
 	//utility stuff here
+	function drawCaret(){
+		//draw a caret
+		overlayContext.fillStyle = caret.color;
+		overlayContext.beginPath();
+		overlayContext.moveTo(caret.x, caret.y - (caret.height/2));
+		overlayContext.lineTo(caret.x + caret.width, caret.y);
+		overlayContext.lineTo(caret.x, caret.y + (caret.height/2));
+		overlayContext.fill();
+	}
 	
+	/**
+	 * This stuff is borrowed from a stack overflow answer: http://stackoverflow.com/questions/2936112/text-wrap-in-a-canvas-element
+	 * originally answered by mizar and edited by Paul Woolcock
+	 * Divide an entire phrase in an array of phrases, all with the max pixel length given.
+	 * The words are initially separated by the space char.
+	 * @param phrase
+	 * @param length
+	 * @return
+	 */
+	var textLines = function (phrase,maxPxLength) {
+	    var wordArray = phrase.split(" "),
+	        phraseArray=[],
+	        lastPhrase="",
+	        l=maxPxLength,
+	        measure=0;
+	    for (var i=0;i<wordArray.length;i++) {
+	        
+	        var word=wordArray[i];
+	        
+	        measure=scriptContext.measureText(lastPhrase+word).width;
+	        	        
+	        if (measure<l) {
+	            lastPhrase+=(" "+word);
+	        }
+	        else {
+	            phraseArray.push(lastPhrase);
+	            lastPhrase=word;
+	        }
+	        
+	        if (i===wordArray.length-1) {
+	            phraseArray.push(lastPhrase);
+	            break;
+	        }
+	    }
+	    return phraseArray;
+	}
+
 	//view stuff here
 	
-	//event stuff here
+	//key event stuff here
+	//the commands we'll use. none by default.
+	var keys = {};
+	
+	//add a command. Pretty simple, but may need to make it do stuff later.
 	var bindKey = function(keycode, func){
+		keys[keycode] = func;
+	};
+	
+	var handleKeyPress = function(e){
+		e = e || window.event;
+		key = e.keyCode;
+		if(typeof keys[key] == 'function'){
+			keys[key]();
+		}
+		//console.log(key + ": " + keys[key]);
+	};
+	
+	//initialization code.
+	var init = function(){
+		document.onkeydown = handleKeyPress;
+
+		window.setInterval(this.draw, 40);
+		
+	};
+	
+	return {
+		init: init,
+		bindKey: bindKey,
+		flipHorizontal: flipHorizontal,
+		flipVertical: flipVertical,
+		stopStart: stopStart,
+		caret: caret,
+		draw: draw,
 		
 	};
 	
